6a2a977658bab34044ac130f0a1ac4e34d94716c
Fabián Cruz <0f.cruz0@gmail.com>
2026-05-12 15:18:19 -0400

    act music-data

---
+++ libro/assets/js/music-data.js
@@ -0,0 +1,16 @@
+// Este es tu archivo central de música. 
+// Cualquier cambio aquí se reflejará en el Dashboard y en la Playlist.
+// Archivo: assets/js/music-data.js
+
+const playlistData = [
+    { id: '9A-XPAv1n3g', title: 'Luz de Día', artist: 'Nuestra Favorita ❤️' },
+    { id: 'c0Ey7nVhdrU', title: 'Recuerdos Contigo', artist: 'Fuego de Noche, Nieve de Día' },
+    { id: 'HJZmbH8qiXQ', title: 'Para Siempre', artist: 'Bella Idiota - Tu Recuerdo' },
+    { id: 'ue0RP3C1Brg', title: 'Nunca lo olvides', artist: 'Me trae directo a ti' },
+    { id: 'T81L7Udz7S8', title: 'Contigo Aprendí', artist: 'Un recuerdo más tuyo' },
+    { id: 'NvR60Wg9R7Q', title: 'Momentos Mágicos', artist: 'Bed of Roses - Encuentros' },
+    { id: 'jhMiIUb2GEA', title: 'A tu lado', artist: 'Primera Cita - Especial' },
+    { id: 'kdfkr1WEzIc', title: 'Por mil Noches', artist: 'En silencio cuando la escuchas' },
+    { id: 'FZednkENwvg', title: 'Complice Eterno', artist: 'Mi compañera' },
+    { id: 'kWwUVBBakC4', title: 'Algo contigo', artist: 'Algo contigo' }
+];
